60678635fb202dc4745bf5a194ba9467d2d7f245
Alikhan Abutalipov <13288276+alikhan-ab@users.noreply.github.com>
2026-01-12 10:53:32 +0500

    Add keymap for the neotree toggle

--- configs/nvim-0.11/plugin/20_keymaps.lua
+++ configs/nvim-0.11/plugin/20_keymaps.lua
@@ -118,7 +118,7 @@ nmap_leader('eo', edit_plugin_file('10_options.lua'),       'Options config')
 nmap_leader('ep', edit_plugin_file('40_plugins.lua'),       'Plugins config')
 nmap_leader('eq', explore_quickfix,                         'Quickfix list')
 nmap_leader('eQ', explore_locations,                        'Location list')
-
+nmap_leader('et', '<Cmd>Neotree toggle<CR>',                'Toggle Neotree')
 -- f is for 'Fuzzy Find'. Common usage:
 -- - `<Leader>ff` - find files; for best performance requires `ripgrep`
 -- - `<Leader>fg` - find inside files; requires `ripgrep`
